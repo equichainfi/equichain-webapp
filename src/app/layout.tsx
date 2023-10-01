b574d379408dd5ff0356fdb5104c5636f4c48582
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-10-01 19:06:26 +0200

    EQC-20 Prettier added + json scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,18 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "EquiChain",
-	description: "Real Estate on Blockchain",
+    title: "EquiChain",
+    description: "Real Estate on Blockchain",
 };
 
 export default function RootLayout({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-	return (
-		<html lang='en'>
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-	return <main>EquiChain</main>;
+    return <main>EquiChain</main>;
 }
