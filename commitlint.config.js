10449c8df0accfcfe8d79770e4372cc327cf28d6
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-10-02 22:24:53 +0200

    EQC-20 husky commit-msg added

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        //     "header-max-length": [0, "always", 100],
+        // },
+        // parserPreset: {
+        //     parserOpts: {
+        //         issuePrefixer: "EQC-",
+        //     },
+    },
+};
