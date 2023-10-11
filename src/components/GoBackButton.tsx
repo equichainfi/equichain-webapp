5897a0d0a050e976e959856f351d0f8c83311646
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-10-11 09:28:59 +0200

    EQC-38 shadcn_ui installed + button component

---
+++ src/components/GoBackButton.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React, { FC } from "react";
+import { buttonVariants } from "./ui/button";
+import { ChevronLeft } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+interface GoBackButtonProps {}
+
+const GoBackButton: FC<GoBackButtonProps> = () => {
+    const pathName = usePathname();
+
+    return (
+        <a href={"/"} className={`${buttonVariants({ variant: "default" })}`}>
+            <ChevronLeft className="h-4 w-4 mr-1" />
+            {pathName === "/" ? "Home" : "Go Back"}
+        </a>
+    );
+};
+
+export default GoBackButton;
