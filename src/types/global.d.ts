2aabcf5564025b7a8785b2666a967aab865c7d4a
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-10-11 14:05:16 +0200

    EQC-39 global types file

---
+++ src/types/global.d.ts
@@ -0,0 +1,26 @@
+export {};
+
+declare global {
+    interface Post {
+        title: string;
+        slug: string;
+        desription: string;
+
+        status: string; //todo enum
+        isVerified: boolean;
+        price: number;
+        chain: string; //todo enum
+
+        area: string;
+
+        createdAt: string; //todo date
+        updatedAt: string; //todo date
+
+        purpose: string; //todo enum
+
+        views: number;
+        saves: number;
+        lastViewed: string; //todo date
+        listedBy: string; //todo enum (user, admin)
+    }
+}
