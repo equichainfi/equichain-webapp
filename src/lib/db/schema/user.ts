5e5b54c105328c677459ba0ffafe56ed36456cb5
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-10-12 20:38:32 +0200

    EQC-40 export fix

--- src/lib/db/schema/user.ts
+++ src/lib/db/schema/user.ts
@@ -1,5 +1,19 @@
-import { pgTable, serial } from "drizzle-orm/pg-core";
+import { date, pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import { Session, Account, Post } from ".";
 
-export const user = pgTable("user", {
+export const User = pgTable("User", {
     id: serial("id").primaryKey(),
+    name: varchar("name", { length: 50 }).notNull(),
+    email: varchar("email", { length: 256 }).notNull().unique(),
+    emailVerified: date("email_verified").notNull(),
+
+    username: varchar("username", { length: 64 }).notNull().unique(),
+    image: varchar("image", { length: 256 }).notNull(),
+
+    accounts: Account.array().notNull(),
+    sessions: Session.array().notNull(),
+    post: Post.array().notNull(),
+    // savedPost:
+    // comment:
+    // likedComment:
 });
