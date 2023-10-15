4f8f749f1e06753728dc71fe94d337b3b70db2ab
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-10-15 11:45:32 +0200

    EQC-42 style class added + fix w/ folder structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
 import GoBackButton from "./GoBackButton";
 import Post from "./Post";
 
-export { GoBackButton, Post };
+import Header from "./constants/Header";
+import Footer from "./constants/Footer";
+
+export { GoBackButton, Post, Header, Footer };
