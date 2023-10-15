7dbd3202de683258bd4b67c8a0a060169d75859c
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-10-15 20:35:24 +0200

    EQC-43 header FE started + exports + few components from UI added

--- src/components/constants/Header.tsx
+++ src/components/constants/Header.tsx
@@ -1,3 +1,74 @@
+"use client";
+
+import { Menu } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import {
+    Button,
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "..";
+import { FullWhite } from "../../../public";
+
 export default function Header() {
-    return <div className="header">Header</div>;
+    const router = useRouter();
+
+    return (
+        <div className="container-flex header">
+            {/* Need logo resolution fix */}
+            <Image
+                src={FullWhite}
+                className="cursor-pointer w-56 sm:w-72"
+                onClick={() => router.push("/")}
+                alt="logo"
+                width={1}
+            />
+
+            <Sheet>
+                <SheetTrigger className="flex md:hidden">
+                    <Menu
+                        size={30}
+                        className="hover:text-primary transition ease-in-out"
+                    />
+                </SheetTrigger>
+                <SheetContent
+                    className="w-screen h-screen bg-white"
+                    side={"top"}
+                >
+                    <SheetHeader>
+                        <SheetTitle>
+                            <Image
+                                src={FullWhite}
+                                className="cursor-pointer"
+                                onClick={() => router.push("/")}
+                                alt="logo"
+                                width={220}
+                            />
+                        </SheetTitle>
+                        <SheetDescription className="grid grid-cols-2 sm:grid-cols-2 place-content-start">
+                            <Button>Button x</Button>
+                            <Button>Button x</Button>
+                            <Button>Button x</Button>
+                            <Button>Button x</Button>
+                            <Button>Button x</Button>
+                            <Button>Button x</Button>
+                            <Button>Button x</Button>
+                            <Button>Button x</Button>
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
+
+            <section className="hidden md:flex items-center justify-between gap-x-16 lg:gap-x-20">
+                <p>Hello</p>
+                <p>Hello</p>
+                <p>Hello</p>
+            </section>
+            <h1 className="hidden md:flex">Account</h1>
+        </div>
+    );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,29 @@ import Post from "./Post";
 import Header from "./constants/Header";
 import Footer from "./constants/Footer";
 
-export { GoBackButton, Post, Header, Footer };
+// UI
+import { ScrollArea } from "./ui/ScrollArea";
+import { Button } from "./ui/Button";
+import { Sheet } from "./ui/sheet";
+import { SheetTrigger } from "./ui/sheet";
+import { SheetContent } from "./ui/sheet";
+import { SheetHeader } from "./ui/sheet";
+import { SheetTitle } from "./ui/sheet";
+import { SheetDescription } from "./ui/sheet";
+import { Separator } from "./ui/separator";
+
+export {
+    GoBackButton,
+    Post,
+    Header,
+    Footer,
+    ScrollArea,
+    Button,
+    Sheet,
+    SheetTrigger,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+    SheetDescription,
+    Separator,
+};
