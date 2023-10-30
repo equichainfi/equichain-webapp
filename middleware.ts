2a7005799feeacd728350c4b8946018f82bcfb9e
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-10-30 23:14:54 +0100

    EQC-56

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+    publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!,
+    publicRoutes: ["/"],
+});
 
 export const config = {
     matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
