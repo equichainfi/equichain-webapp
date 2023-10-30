ea298d0a0750ab567a643d85f0d212a591d51647
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-10-30 23:04:33 +0100

    EQC-56

--- src/lib/db/schema/comment.ts
+++ src/lib/db/schema/comment.ts
@@ -21,26 +21,3 @@ export const Comment = pgTable("Comment", {
     // likes:
     commentId: text("comment_id"),
 });
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
-
-export const Comment = pgTable("Comment", {
-    id: serial("id").primaryKey(),
-    text: text("text").notNull(),
-    createdAt: timestamp("created_at").notNull(),
-    // author: User.references(() => User, "author_id", "id", "author").notNull(),
-    // authorId: text("author_id").notNull(),
-    // post: User.references(() => User, "post_id", "id", "post").notNull(),
-    postId: text("post_id").notNull(),
-
-    replyToId: text("reply_to_id"),
-    // replyTo: Comment.references(() => Comment, "reply_to_id", "id", "reply_to"),
-    // replies: Comment.array().references(
-    //     () => Comment,
-    //     "reply_to_id",
-    //     "id",
-    //     "replies",
-    // ),
-
-    // likes:
-    commentId: text("comment_id"),
-});
