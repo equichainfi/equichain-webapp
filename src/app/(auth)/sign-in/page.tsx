8e159f65c6f89a4c7513e34f32b7c969a04646dc
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-11-12 21:49:37 +0100

    EQC-63 file name fix for vercel

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,9 +3,8 @@ import { buttonVariants } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
-import { FC } from "react";
 
-const page: FC = () => {
+const page = () => {
     return (
         <div className="absolute inset-0">
             <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
