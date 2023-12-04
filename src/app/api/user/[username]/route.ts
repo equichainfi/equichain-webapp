e3bf4fc05d504028270c2d35a4776b863c028e87
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-12-04 19:20:35 +0100

    EQC-78 fix

--- src/app/api/user/[username]/route.ts
+++ src/app/api/user/[username]/route.ts
@@ -3,21 +3,22 @@
 // import { FullQueryResults } from "@neondatabase/serverless";
 // import { sql } from "drizzle-orm";
 // import { notFound } from "next/navigation";
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export async function GET() {
-//     // const { username } = req.query;
-//     // console.log(username);
+export async function GET() {
+    // const { username } = req.query;
+    // console.log(username);
 
-//     try {
-//         const user: FullQueryResults<boolean> = await db.execute(
-//             sql`select * from ${User} where username = olivier`,
-//         );
-//         console.log(user);
-//         if (!user) return notFound();
+    // try {
+    //     const user: FullQueryResults<boolean> = await db.execute(
+    //         sql`select * from ${User} where username = olivier`,
+    //     );
+    //     console.log(user);
+    //     if (!user) return notFound();
 
-//         return new NextResponse(JSON.stringify(user), { status: 200 });
-//     } catch (error) {
-//         return new NextResponse(JSON.stringify({}), { status: 500 });
-//     }
-// }
+    //     return new NextResponse(JSON.stringify(user), { status: 200 });
+    // } catch (error) {
+    //     return new NextResponse(JSON.stringify({}), { status: 500 });
+    // }
+    return new NextResponse(JSON.stringify({}), { status: 200 });
+}
