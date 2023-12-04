763666a30e22d7ae03aa7376469636afb5b269e6
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-12-04 18:58:36 +0100

    EQC-79 fix

--- src/components/constants/connect/ConnectWalletButton.tsx
+++ src/components/constants/connect/ConnectWalletButton.tsx
@@ -6,47 +6,32 @@ import React from "react";
 import ConnectWalletWelcomePage from "./ConnectWalletWelcomePage";
 
 export default function ConnectWalletButton() {
-    return (
-        <>
-            <div className="hidden md:block">
-                <ConnectWallet
-                    theme={lightTheme({
-                        colors: {
-                            accentText: PRIMARY_COLOR,
-                            accentButtonBg: PRIMARY_COLOR,
-                        },
-                    })}
-                    btnTitle={"Connect Wallet"}
-                    modalTitle={"Connect"}
-                    auth={{ loginOptional: false }}
-                    switchToActiveChain={true}
-                    modalSize={"wide"}
-                    welcomeScreen={() => <ConnectWalletWelcomePage />}
-                    modalTitleIconUrl={"/public/images/logo.svg"}
-                    termsOfServiceUrl={"/w/terms"}
-                    privacyPolicyUrl={"/w/safety"}
-                />
-            </div>
-            <div className="md:hidden block">
-                <ConnectWallet
-                    theme={lightTheme({
-                        colors: {
-                            accentText: PRIMARY_COLOR,
-                            accentButtonBg: PRIMARY_COLOR,
-                        },
-                    })}
-                    btnTitle={"Connect Wallet"}
-                    modalTitle={"Connect"}
-                    auth={{ loginOptional: false }}
-                    switchToActiveChain={true}
-                    modalSize={"compact"}
-                    modalTitleIconUrl={"/public/images/logo.svg"}
-                    termsOfServiceUrl={"/w/terms"}
-                    privacyPolicyUrl={"/w/safety"}
-                />
-            </div>
-        </>
-    );
+    const showClassNames = [
+        { className: "hidden md:block" },
+        { className: "md:hidden block" },
+    ];
+    return showClassNames.map((wallet, index: number) => (
+        <div key={index} className={wallet.className}>
+            <ConnectWallet
+                className="connect-wallet-button"
+                theme={lightTheme({
+                    colors: {
+                        accentText: PRIMARY_COLOR,
+                        accentButtonBg: PRIMARY_COLOR,
+                    },
+                })}
+                btnTitle={"Connect"}
+                modalTitle={"Connect"}
+                auth={{ loginOptional: true }}
+                switchToActiveChain={true}
+                modalSize={"wide"}
+                welcomeScreen={() => <ConnectWalletWelcomePage />}
+                modalTitleIconUrl={"/public/images/logo.svg"}
+                termsOfServiceUrl={"/w/terms"}
+                privacyPolicyUrl={"/w/safety"}
+            />
+        </div>
+    ));
 }
 
 //! Connect Button can be customized only in css with !important
