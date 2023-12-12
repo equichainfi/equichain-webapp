1e231bfab0d9de763082351bf45e624d3214963a
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-12-12 14:13:30 +0100

    EQC-73 UI fix

--- src/components/dashboard/user/UserMain.tsx
+++ src/components/dashboard/user/UserMain.tsx
@@ -8,18 +8,24 @@ import {
     TooltipProvider,
     TooltipTrigger,
 } from "@/components";
+import clsx from "clsx";
 import {
     Copy,
     Flag,
     HeartHandshake,
     MoreHorizontal,
     Share,
+    Twitter,
 } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
+import { SmallLogoPlain } from "../../../../public";
+import copy from "clipboard-copy";
+import { usePathname } from "next/navigation";
 
 export default function UserMain() {
-    const [isVisible, setIsVisible] = useState<boolean>(false);
+    const [isVisibleOptions, setIsVisibleOptions] = useState<boolean>(false);
+    const [isVisibleShare, setIsVisibleShare] = useState<boolean>(false);
     const userOptions: string[] = [
         "Collected",
         "Offers made",
@@ -29,17 +35,39 @@ export default function UserMain() {
         "Favorites",
         "Activity",
     ];
-
+    const [selectedOption, setSelectedOption] = useState<string>(
+        userOptions[0],
+    );
+    const pathname: string = usePathname();
+    const [profileBanner, setProfileBanner] = useState<string>(
+        "/images/full_black.svg",
+    );
     return (
         <main className="w-screen">
             <header className="w-full">
                 <section className="flex flex-col">
                     <Image
-                        src="/images/full_black.svg"
+                        src={profileBanner}
                         alt="Logo"
                         width={100}
                         height={100}
-                        className="w-screen min-h-[120px] max-h-[330px] object-center object-cover"
+                        className="w-screen min-h-[120px] max-h-[160px] md:max-h-[200px] lg:max-h-[225px] xl:max-h-[325px] object-center object-cover cursor-pointer"
+                        onClick={() => {
+                            const input: HTMLInputElement =
+                                document.createElement("input");
+                            input.type = "file";
+                            input.accept = ".jpg, .png";
+                            input.onchange = (e) => {
+                                const file: File | undefined = (
+                                    e.target as HTMLInputElement
+                                )?.files?.[0];
+                                const reader: FileReader = new FileReader();
+                                reader.onloadend = () =>
+                                    setProfileBanner(reader.result as string);
+                                if (file) reader.readAsDataURL(file);
+                            };
+                            input.click();
+                        }}
                     />
                     <div className="ml-2 my-5 flex sm:ml-10 lg:ml-16 xl:ml-20 2xl:ml-24 items-start justify-start absolute top-24 md:top-36 lg:top-44 xl:top-58 2xl:top-64 3xl:top-72 bg-white rounded-full">
                         <Image
@@ -53,7 +81,14 @@ export default function UserMain() {
                     <section>
                         <div className="container-flex pb-0 flex gap-3 items-center justify-between">
                             <h1 className="font-semibold text-3xl">Olivier</h1>
-                            <section>
+                            <section
+                                className={
+                                    (clsx(""),
+                                    pathname === "/account"
+                                        ? "hidden"
+                                        : "block")
+                                }
+                            >
                                 <TooltipProvider>
                                     <Tooltip>
                                         <TooltipTrigger className="h-auto w-auto rounded-lg text-center p-2">
@@ -67,21 +102,64 @@ export default function UserMain() {
                                 <TooltipProvider>
                                     <Tooltip>
                                         <TooltipTrigger className="h-auto w-auto text-center p-2">
-                                            <Share size={30} />
+                                            <button
+                                                onClick={() => {
+                                                    setIsVisibleShare(
+                                                        !isVisibleShare,
+                                                    );
+                                                    setIsVisibleOptions(false);
+                                                }}
+                                            >
+                                                <Share size={30} />
+                                            </button>
                                         </TooltipTrigger>
                                         <TooltipContent className="bg-white">
                                             <p>Share</p>
                                         </TooltipContent>
                                     </Tooltip>
                                 </TooltipProvider>
+                                {isVisibleShare && (
+                                    <div className="bg-white rounded-xl border py-4 px-2 absolute top-70 right-3 sm:right-10 md:right-16 lg:right-20 2xl:right-24 w-[220px] h-[210px] md:h-[140px] flex flex-col gap-y-5 z-50">
+                                        <div className="flex flex-col items-center justify-start">
+                                            <button
+                                                className="w-full h-14 rounded-xl hover:bg-zinc-100 flex items-center justify-start gap-2 pl-3"
+                                                onClick={() =>
+                                                    copy(
+                                                        `www.equichain.fi${pathname}`,
+                                                    )
+                                                }
+                                            >
+                                                <Image
+                                                    src={SmallLogoPlain}
+                                                    alt="Logo"
+                                                    width={20}
+                                                    height={20}
+                                                    className="object-contain"
+                                                />
+                                                <p className="font-semibold">
+                                                    Copy link
+                                                </p>
+                                            </button>
+                                            <button className="w-full h-14 rounded-xl hover:bg-zinc-100 flex items-center justify-start gap-2 pl-3">
+                                                <Twitter size={20} />
+                                                <p className="font-semibold">
+                                                    Share on Twitter
+                                                </p>
+                                            </button>
+                                        </div>
+                                    </div>
+                                )}
                                 <button
-                                    onClick={() => setIsVisible(!isVisible)}
+                                    onClick={() => {
+                                        setIsVisibleOptions(!isVisibleOptions);
+                                        setIsVisibleShare(false);
+                                    }}
                                     className="h-auto w-auto text-center p-2"
                                 >
                                     <MoreHorizontal size={30} />
                                 </button>
-                                {isVisible && (
-                                    <div className="bg-white rounded-xl border py-5 px-2 absolute top-70 right-3 sm:right-10 md:right-16 lg:right-20 2xl:right-24 w-[220px] h-[210px] md:h-[125px] flex flex-col gap-y-5">
+                                {isVisibleOptions && (
+                                    <div className="bg-white rounded-xl border py-5 px-2 absolute top-70 right-3 sm:right-10 md:right-16 lg:right-20 2xl:right-24 w-[220px] h-[210px] md:h-[125px] flex flex-col gap-y-5 z-50">
                                         <div className="md:hidden block bg-white">
                                             <h3 className="font-semibold text-zinc-700 text-sm pb-5 pl-3">
                                                 ADRESSES
@@ -130,7 +208,15 @@ export default function UserMain() {
                                 (option: string, index: number) => (
                                     <button
                                         key={index}
-                                        className="px-3 sm:px-4 py-2 sm:py-3 hover:bg-zinc-100 rounded-xl flex items-center justify-center font-medium sm:text-lg"
+                                        className={clsx(
+                                            "px-3 sm:px-4 py-2 sm:py-3 hover:bg-zinc-100 rounded-xl flex items-center justify-center font-medium sm:text-lg",
+                                            selectedOption === option
+                                                ? "bg-zinc-100"
+                                                : "bg-white",
+                                        )}
+                                        onClick={() => {
+                                            setSelectedOption(option);
+                                        }}
                                     >
                                         {option}
                                     </button>
